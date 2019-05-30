11be4595e7be21367493cfffb637a0a1be48c2d4
rehten <Sithas@yandex.ru>
2019-05-30 18:37:42 +0300

    Start - render multiple list

---
+++ src/app/modules/shared/components/header/header-input/header-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ndv-header-input',
+  templateUrl: './header-input.component.html',
+  styleUrls: ['./header-input.component.scss']
+})
+export class HeaderInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
